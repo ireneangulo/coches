827384342dd4da6dd6d1c1710ce6955654b80d1b
ireneangulo <irene.anguloeso@gmail.com>
2022-10-26 09:56:20 +0200

    clase coche

--- ejercicio.py
+++ ejercicio.py
@@ -5,4 +5,12 @@ class Vehiculo():
     
     def __str__(self):
         return "color {}, {} ruedas".format(self.color, self.ruedas)
-        
+    
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return "color {}, {} km/h, {} ruedas, {} cc".format(self.color, self.velocidad, self.ruedas, self.cilindrada)
