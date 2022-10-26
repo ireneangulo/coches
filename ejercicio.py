89137498913ef15841034e394b623fdec3a3b2bb
ireneangulo <irene.anguloeso@gmail.com>
2022-10-26 10:06:10 +0200

    he hecho la clase camioneta

--- ejercicio.py
+++ ejercicio.py
@@ -4,13 +4,23 @@ class Vehiculo():
         self.ruedas = ruedas
     
     def __str__(self):
-        return "color {}, {} ruedas".format(self.color, self.ruedas)
+        return "Color {}, {} ruedas".format(self.color, self.ruedas)
     
 class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada):
-        self.color = color
-        self.ruedas = ruedas
+        Vehiculo.__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
     def __str__(self):
-        return "color {}, {} km/h, {} ruedas, {} cc".format(self.color, self.velocidad, self.ruedas, self.cilindrada)
+        return Vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
+
+coche = Coche("azul", 150, 4, 1200)
+print(coche)
+
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga= carga
+    def __str__(self):
+        return super().__str__() + ", {} kg de carga".format(self.carga)
+
