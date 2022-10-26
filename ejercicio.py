00ee59bf8a0da43fc75242f98aac042274d438a6
ireneangulo <irene.anguloeso@gmail.com>
2022-10-26 10:11:51 +0200

    clase motocicleta

--- ejercicio.py
+++ ejercicio.py
@@ -31,3 +31,10 @@ class Bicicleta(Coche):
     def __str__(self):
         return super().__str__() + ", {}".format(self.tipo)
 
+class Motocicleta(Coche):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas, tipo)
+        self.velocidad = velocidad
+        self.cilindrada= cilindrada
+    def __str__(self):
+        return super().__str__() + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
